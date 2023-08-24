9063d5fc419876a1b36153785c46cbaf0552e0ed
__Saritah__ <142619907+Saramarinho@users.noreply.github.com>
2023-08-24 17:31:59 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+// this is a js file
+ //Sara Marinho
+
+ //A.Exercícios de interpretação de códigos
+/* a. false
+   b. false
+   c. true
+   d. boolean
+   
+//2. resposta
+//* falta chamar o método number
+//* vai ser impresso os dois numeros um ao lado do outro.
+
+   // 3. resposta
+let primeiroNumero = Number(prompt("Digite um numero!"));
+let segundoNumero = Number(prompt("Digite outro numero!"));
+const soma = primeiroNumero + segundoNumero;
+console.log(soma)
+*/
+B 1.
+let usuario = Number(prompt("Qual a sua idade?"));
+let melhorAmigo = Number(prompt("Qual a idade do seu melhor amigo?"));
+console.log("Sua idade é maior do que a do seu melhor amigo?", (usuario > melhorAmigo));
+console.log(usuario - melhorAmigo);
+
+//B 2.
+let numero = Number(prompt("Digite um numero par"));
+console.log(numero % 2);
+
+//B 3.
+
+let idade = Number(prompt("Qual a sua idade?"));
+let idadeMeses = idade * 12;
+let idadeDias = idadeMeses * 30;
+let idadeHoras = idadeDias * 24;
+console.log(idadeMeses, idadeDias, idadeHoras);
+//--------------------------
+console.log(idadeMeses);
+console.log(idadeDias);
+console.log(idadeHoras);
+
+//B 4.
+let numeroUm = Number(prompt("Digite um numero"));
+let numeroDois = Number(prompt("Digite outro numero"
+/*B.Exercícios de escrita de código
+*/
+let idadeU = Number(prompt("Qual a sua idade?"));
+let idadeA = Number(prompt("Idade do seu melhor amigo?"));
+console.log("Sua idade é maior do que a do seu melhor amigo?", (idadeU > idadeA));
+console.log(idadeU - idadeA);
+//*
+let number = Number(prompt("insira um número par"));
+console.log(number % 2);
+//*
+let idadeY = prompt = ("Qual sua idade em anos");
+let idadeM = (idadeY * 12);
+let idadeD = (idadeY * 30);
+let idadeH = (idadeH * 24);
+console.log(idadeY, idadeM, idadeD, idadeH);
+
+
+
+
+
